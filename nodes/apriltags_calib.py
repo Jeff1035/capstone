eca657d14a8dcef9ee6939b340d9cdb8b878853b
Tyler <tchesebro@berkeley.edu>
2017-04-21 17:01:00 -0700

    further apriltag_calib

--- nodes/apriltags_calib.py
+++ nodes/apriltags_calib.py
@@ -4,10 +4,10 @@ import numpy as np
 import message_filters
 from std_msgs.msg import String
 from geometry_msgs.msg import Transform
-from visualization_msgs.msg import MarkerArray
+from visualization_msgs.msg import MarkerArray, Marker
 import transformations as tf
 
-pub = rospy.Publisher('/apriltags/extcalib/', Transform, queue_size=10)
+pub = rospy.Publisher('/apriltags/extcalib/', MarkerArray, queue_size=10)
 extcalib = None
 res = None
 
@@ -28,26 +28,26 @@ def getExt():
 	print "calibration finished"
 
 def transform_callback(mkarr):
+	ret_array = MarkerArray()
+	print "Detected {0} AprilTags".format(len(mkarr.markers))
+
 	for m in mkarr.markers:
+		marker = Marker()
 		matrix = tf.quaternion_matrix([m.pose.orientation.w,
 										m.pose.orientation.x, 
 										m.pose.orientation.y,
 										m.pose.orientation.z])
 		rotation_matrix = matrix[:3,:3]
-		translation = np.zeros(3)
-		translation[0] = m.pose.position.x
-		translation[1] = m.pose.position.y
-		translation[2] = m.pose.position.z
-		camera_t = extcalib[:3,3]
-		#print camera_t
-		camera_R = extcalib[:3,:3]
-		#print camera_R
-		# print rotation_matrix
-		# print translation
-		t = camera_t + translation
-		R = rotation_matrix.dot(camera_R)
-		print t
-		print R
+		translation = np.array([m.pose.position.x, m.pose.position.y, m.pose.position.z])
+		t = extcalib[:3, 3] + translation
+		R = extcalib[:3, :3].dot(camera_R)
+		marker.pose.position.x = t[0]
+		marker.pose.position.y = t[1]
+		marker.pose.position.z = t[2]
+		#TODO: add the correct orientatoins for each pose
+		ret_array.markers.append(marker)
+
+	pub.publish(ret_array)
 
 def april_calib():
 	print "searching for apriltags...."
